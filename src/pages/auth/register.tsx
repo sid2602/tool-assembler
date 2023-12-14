06118cfb6761fc6443bc979ba2a375caf59673ad
sid2602 <sidhd2602@gmail.com>
2023-12-14 15:25:46 +0100

    add basic promo page

--- src/pages/auth/register.tsx
+++ src/pages/auth/register.tsx
@@ -62,65 +62,69 @@ export default function Register() {
 					</CardHeader>
 
 					<CardBody>
-						<Stack>
-							<FormControl isInvalid={isEmailInvalid}>
-								<FormLabel>Email</FormLabel>
-								<Input
-									placeholder="Please enter email"
-									name="email"
-									onChange={formik.handleChange}
-									onBlur={formik.handleBlur}
-									value={formik.values.email}
-								/>
+						<form onSubmit={formik.handleSubmit}>
+							<Stack>
+								<FormControl isInvalid={isEmailInvalid}>
+									<FormLabel>Email</FormLabel>
+									<Input
+										placeholder="Please enter email"
+										name="email"
+										onChange={formik.handleChange}
+										onBlur={formik.handleBlur}
+										value={formik.values.email}
+									/>
 
-								<FormErrorMessage>{formik.errors.email}</FormErrorMessage>
-							</FormControl>
-							<FormControl isInvalid={isFirstNameInvalid}>
-								<FormLabel>First name</FormLabel>
-								<Input
-									placeholder="Please enter first name"
-									name="firstName"
-									onChange={formik.handleChange}
-									onBlur={formik.handleBlur}
-									value={formik.values.firstName}
-								/>
+									<FormErrorMessage>{formik.errors.email}</FormErrorMessage>
+								</FormControl>
+								<FormControl isInvalid={isFirstNameInvalid}>
+									<FormLabel>First name</FormLabel>
+									<Input
+										placeholder="Please enter first name"
+										name="firstName"
+										onChange={formik.handleChange}
+										onBlur={formik.handleBlur}
+										value={formik.values.firstName}
+									/>
 
-								<FormErrorMessage>{formik.errors.firstName}</FormErrorMessage>
-							</FormControl>
-							<FormControl isInvalid={isLastNameInvalid}>
-								<FormLabel>Last name</FormLabel>
-								<Input
-									placeholder="Please enter last name"
-									name="lastName"
-									onChange={formik.handleChange}
-									onBlur={formik.handleBlur}
-									value={formik.values.lastName}
-								/>
+									<FormErrorMessage>{formik.errors.firstName}</FormErrorMessage>
+								</FormControl>
+								<FormControl isInvalid={isLastNameInvalid}>
+									<FormLabel>Last name</FormLabel>
+									<Input
+										placeholder="Please enter last name"
+										name="lastName"
+										onChange={formik.handleChange}
+										onBlur={formik.handleBlur}
+										value={formik.values.lastName}
+									/>
 
-								<FormErrorMessage>{formik.errors.lastName}</FormErrorMessage>
-							</FormControl>
-							<FormControl isInvalid={isPasswordInvalid}>
-								<FormLabel>Password</FormLabel>
-								<Input
-									placeholder="Please enter password"
-									name="password"
-									onChange={formik.handleChange}
-									onBlur={formik.handleBlur}
-									value={formik.values.password}
-									type="password"
-								/>
-								<FormErrorMessage>{formik.errors.password}</FormErrorMessage>
-							</FormControl>
-							<Button mt="2">Sign up</Button>
-						</Stack>
-						<Text fontSize="sm">
-							Already have account?{" "}
-							<Link href={"/auth/login"} passHref>
-								<Text as={"span"} color="teal.500">
-									Login now
-								</Text>
-							</Link>
-						</Text>
+									<FormErrorMessage>{formik.errors.lastName}</FormErrorMessage>
+								</FormControl>
+								<FormControl isInvalid={isPasswordInvalid}>
+									<FormLabel>Password</FormLabel>
+									<Input
+										placeholder="Please enter password"
+										name="password"
+										onChange={formik.handleChange}
+										onBlur={formik.handleBlur}
+										value={formik.values.password}
+										type="password"
+									/>
+									<FormErrorMessage>{formik.errors.password}</FormErrorMessage>
+								</FormControl>
+								<Button mt="2" type="submit">
+									Sign up
+								</Button>
+							</Stack>
+							<Text fontSize="sm">
+								Already have account?{" "}
+								<Link href={"/auth/login"} passHref>
+									<Text as={"span"} color="teal.500">
+										Login now
+									</Text>
+								</Link>
+							</Text>
+						</form>
 					</CardBody>
 				</Card>
 			</Container>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,54 @@
+import {
+	Box,
+	Button,
+	Center,
+	Container,
+	Flex,
+	Heading,
+	Icon,
+	Image,
+	Stack,
+	Text,
+} from "@chakra-ui/react";
+import { MdOutlineHandyman } from "react-icons/md";
+
 export default function Home() {
 	return (
-		<>
-			<main>Something</main>
-		</>
+		<Flex h="100vh" justifyContent="center">
+			<Container maxW="container.md" mt="150" p="0">
+				<Stack>
+					<Heading mt="8">
+						<Center>Welcome to Tool builder</Center>
+					</Heading>
+					<Box mt="8">
+						<Center>
+							<Text maxW="md" fontSize="2xl" textAlign="center">
+								Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin
+								luctus augue nec nulla pharetra eleifend. Vivamus dignissim
+								metus eros, id euismod dolor bibendum id.
+							</Text>
+						</Center>
+					</Box>
+					<Image
+						mt="8"
+						mb="5"
+						src="https://picsum.photos/200/300"
+						alt={"tool builder"}
+						maxW="100%"
+						height="250"
+					/>
+					<Center>
+						<Button
+							size="md"
+							width="sm"
+							leftIcon={<Icon as={MdOutlineHandyman} />}
+							rightIcon={<Icon as={MdOutlineHandyman} />}
+						>
+							Create your tool
+						</Button>
+					</Center>
+				</Stack>
+			</Container>
+		</Flex>
 	);
 }
